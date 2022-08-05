4ef5f61a5dfc6975dcb72e20b7bbd0bb73960745
Fabian <fabian.rodrez@gmail.com>
2022-08-04 22:39:06 -0500

    Added chaert for big o

--- src/scripts/generate-sitemap.js
+++ src/scripts/generate-sitemap.js
@@ -16,9 +16,6 @@ const siteMetadata = require('../data/siteMetadata')
     '!pages/_*.tsx',
     '!pages/api',
   ])
-
-  console.log(pages)
-
   const sitemap = `
         <?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
